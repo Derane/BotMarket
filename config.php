0d01a69bde1425bfd7b15616a2d8efbcb4c1917f
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2023-07-20 17:26:53 +0300

    Initial commit

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+const TOKEN = '6395924254:AAEEzoOFp1qilwVkuMMtRgXiElMIILysDz0';
+
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+error_reporting(-1);
+ini_set('display_errors', 0);
+ini_set('log_errors', 'on');
+ini_set('error_log', __DIR__ . '/errors.log');
+
+echo 'hello';
